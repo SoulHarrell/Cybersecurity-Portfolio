f837741fcaedb75e65a2e4967729ea7a5372d08d
Soul <noah.4247@gmail.com>
2026-06-24 07:05:05 -0500

    Update RuleEngine.java

--- Java-Code-Projects/Mini SIEM Log Analyzer/RuleEngine.java
+++ Java-Code-Projects/Mini SIEM Log Analyzer/RuleEngine.java
@@ -1,25 +1,28 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class RuleEngine {
-    HashSet<String> alertedIps = new HashSet<>();
-    
+
+    private HashSet<String> alertedIps = new HashSet<>();
+
     public void analyze(ArrayList<LogEvent> events, AlertManager alertManager) {
-        
+
+        HashMap<String, Integer> ipCounts = new HashMap<>();
+
         for (LogEvent event : events) {
-            
             String ip = event.getIp();
-            int count = 0;
-            
-            for (LogEvent other : events) {
-                
-                if (other.getIp().equals(ip)) {
-                    count++;
-                }
-            }
+            ipCounts.put(ip, ipCounts.getOrDefault(ip, 0) + 1);
+        }
+
+        for (String ip : ipCounts.keySet()) {
+            int count = ipCounts.get(ip);
+
             if (count >= 3 && !alertedIps.contains(ip)) {
-                alertManager.sendAlert("Possible brute force from IP " + ip + " (" + count + " attempts)");
-                
+                alertManager.sendAlert(
+                    "Possible brute force from IP " + ip + " (" + count + " attempts)"
+                );
+
                 alertedIps.add(ip);
             }
         }
